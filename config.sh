b22c468a04d57493909e1a9f4167b9bd366e4825
125998211-myseneca <sjaffarullah@myseneca.ca>
2024-11-15 06:20:35 -0500

    added az account to check az login status

--- config.sh
+++ config.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# setup script
+
+# Check Az login status
+echo "Checking Azure login status..."
+az account show &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "You are not logged into Azure CLI. Initiating login..."
+    az login
+    if [ $? -ne 0 ]; then
+        echo "Error: Azure login failed. Please try again."
+        exit 1
+    fi
+else
+    echo "Azure CLI login detected."
+fi
 
 # Prompt for Azure storage account name and key
 read -p "Enter your Azure Storage Account Name: " AZURE_STORAGE_ACCOUNT
